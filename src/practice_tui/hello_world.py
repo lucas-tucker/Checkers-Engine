ad200dce9c600fd4b6f67b1856bc5b28c43d5e75
Nicholas Peter Bourkoff Matheos <nmatheos@linux5.cs.uchicago.edu>
2023-02-23 11:52:26 -0600

    Figured out a nicer way to print the board, involving better spacing and using the rich library to add colors cooresponding to the pieces.

--- src/practice_tui/hello_world.py
+++ src/practice_tui/hello_world.py
@@ -24,3 +24,42 @@ def print_board():
     b = checkers.Board(3)
     print(b)
     return None
+    
+
+
+"""
+
+some stuff I did. In order for this stuff to work you need to
+from rich.console import Console
+
+console = Console()
+
+in Square class
+    def __str__(self) -> str:
+        """ Returns a string representation of what is on the square."""
+        if self.piece == None:
+            return " [bold yellow]□[/bold yellow] "
+        if self.piece.color is PieceColor.RED:
+            color = "red"
+        else:
+            color = "blue"
+        if self.piece.is_king:
+            return " [green]" + self.piece.color.name[0] + "[/green] "
+
+in board class
+
+    def nice_print(self): # -> str:
+        """
+        Represents the board as a string.
+
+        Returns:
+            str
+        """
+        s = ""
+        for row in self._board:
+            for square in row:
+                s += str(square)
+            s += "\n"
+        console.print(s)
+        return None
+"""
