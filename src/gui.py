220a0444382c648758cb7809888a50116a7d61b7
Althea Yan Li <ayl@linux2.cs.uchicago.edu>
2023-02-24 13:22:10 -0600

    gui basis, nothing is working though

--- src/gui.py
+++ src/gui.py
@@ -5,7 +5,91 @@ Author: Althea Li
 """
 
 import pygame
+from pygame.locals import *
+import click
 
-class GUIPlayer:
+from checkers import Board, Square, Piece, Moves, PieceColor
 
-    def __init__(self)
+from enum import Enum
+
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+
+try:
+    os.environ["DISPLAY"]
+except: 
+    os.environ["SDL_VIDEODRIVER"] = 'windib'
+os.environ['SDL_AUDIODRIVER'] = 'dsp'
+
+
+Board_width = 3 # or range from 6 to 20
+Empty_Grid = "Empty_Grid"
+Grid_pixelsize = 100
+Black = (0, 0, 0)
+Red = ( 255, 0, 0)
+Light_Brown = ( 200, 157, 124)
+Dark_Brown = ( 105, 53, 36)
+Dark_Gray = (100, 100, 100)
+Gray = (180, 180, 180)
+White = (255,255,255)
+Background = Gray
+
+WIDTH = 600
+HEIGHT = 600
+
+def draw_board(surface: pygame.surface.Surface, board):
+    """
+    Draws the state of the board in the window
+    
+    Args:
+        surface: Pygame surface to draw the board on
+        board: The board to draw
+
+    Returns: None
+    """
+    
+    grid = board._board
+    board_dim = board._board_dim
+
+    rh = HEIGHT / (board_dim + 1)
+    cw = WIDTH / (board_dim+1)
+    for row in range(board_dim):
+        for col in range(board_dim):
+            rect = (col * cw, row * rh, cw, rh)
+            if (row + col)%2 == 0:
+                pygame.draw.rect(surface, color=White, rect=rect, width=2)
+            else:
+                pygame.draw.rect(surface, color=Black, rect=rect, width=2)
+
+def play_checkers(board):
+    """
+    Plays a game of checkers on a Pygame window
+
+    Args:
+        board: The board to play on
+
+    Returns: None
+
+    """
+    current = PieceColor.BLACK
+    pygame.init()
+    pygame.display.set_caption("Checkers")
+    """
+    surface = pygame.display.set_mode((WIDTH, HEIGHT))
+    """
+    surface1.fill(Background)
+    clock = pygame.time.Clock()
+
+    draw_board(surface, board)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                raise SystemExit
+        draw_board(surface, board)
+        pygame.display.update()
+        clock.tick(24)
+
+b = Board(3)
+play_checkers(b)
