3d67a3e7433e7c4d7eeb8b60ac8f95fc8875f73b
Nicholas Peter Bourkoff Matheos <nmatheos@linux5.cs.uchicago.edu>
2023-02-23 18:22:06 -0600

    Made asthetic changes to tui board printing. Specifically, I added a number indicator for each board row and a letter indicator for each column. I also changed the formating from open o's to closed circles.

--- src/tui.py
+++ src/tui.py
@@ -5,6 +5,9 @@ Current Status : Currently need to implement get_move functionality.
 
 Done By : Niko
 """
+from typing import Union, Dict
+
+
 from checkers import Board, Square, Piece, Moves
 from rich.console import Console
 from enum import Enum
@@ -55,49 +58,107 @@ class TUIPlayer:
                     # Print prompt with column already filled in
                     print(Style.BRIGHT + f"{self.name}> " + Style.RESET_ALL + str(column+1))
                     return column
+        else:
+            # Ask for a move (and re-ask if a valid move is not provided)
+            while True:
+                v = input(f"{self.name}> ")
+
+                # Check if input is actually a valid move
+                if _______ #condition for valid move :
+                    ###
+                elif v == "Help":
+                    # PRINT VALID MOVES AS TREES #
                 else:
-                    # Ask for a move (and re-ask if a valid move is not provided)
-                    while True:
-                        v = input(Style.BRIGHT + f"{self.name}> " + Style.RESET_ALL)
-                        if len(v) == 1 and v[0] in "1234567":
-                            try:
-                                col = int(v) - 1
-                                if self.board.can_drop(col):
-                                    return col
-                            except ValueError:
-                                continue
-                        if self.board.valid_moves(self.color)
+                    console.print("[bold red]ERROR:[/bold red] not a valid move, \
+                        please try again.")
+                    console.print("[i yellow]Hint: enter Help for a list of \
+                        valid moves[/i yellow]")
+
+                if len(v) == 1 and v[0] in "1234567":
+                    try:
+                        col = int(v) - 1
+                        if self.board.can_drop(col):
+                            return col
+                    except ValueError:
+                        continue
+                if self.board.valid_moves(self.color)
         """
 def print_board(board: Board) -> None:
-    """ Prints the board to the screen
+    """ 
+    Prints the board to the screen.
+
+    In the checkers.py file, there exists a __str__ method, which produces a
+    string representation of the board. print_board uses this and then adds
+    color and spacing to make the board more comprehensible.
+
     Args:
         board: The board to print
     Returns: None
     """
+    size = board._board_dim  - 1
     s = []
-    for row in board:
+    count = size
+    for row in board._board:
+        # On each new row we add a number and a divider (ex. 1| )
+        if count >= 100:
+            s.append(str(count))
+        elif count >= 10:
+            s.append(" " + str(count))
+        else:
+            s.append("  " + str(count))
+        s.append("│")
         for square in row:
+            # For each row we then add all the string rep of the squares (either
+            # a piece or a blank)
             s.append(str(square))
+        # To end each row we add a new line and decrease our row number
         s.append("\n")
+        count += -1
+
     final_s = ""
     for char in s:
+        # For each character of our string representation of the board, we then
+        # create a newly formatted representation.
         if char == "□":
-            final_s += " [bold]□[/bold] "
+            final_s += " ■ "
         elif char == "b":
-            final_s += " [blue]o[/blue] "
+            final_s += " [blue]●[/blue] "
         elif char == "r":
-            final_s += " [red]o[/red] "
+            final_s += " [red]●[/red] "
         elif char == "B":
-            final_s += " [blue]O[/blue] "
+            final_s += " [blue]◎[/blue] "
         elif char == "R":
-            final_s += " [red]O[/red] "
+            final_s += " [red]◎[/red] "
         else:
             final_s += char
-    console.print(final_s)
+    # The bulk of the board is now created, we just need to add the bottom count
+
+    alph = "abcdefghijklmnopqrstuvwxyz"
+    bottom = []
+    for num in range(0, size + 5):
+        # First we add a full row of dividers
+        if num <= 2:
+            # We don't want a divider below the other set of numbers
+            bottom.append(" ")
+        elif num == 3:
+            # We want to connect our dividers
+            bottom.append("└")
+        else:
+            # Finish dividers
+            bottom.append("───")
+    bottom.append("\n")
+    for num in range(0, size + 5):
+        # Now we add the numbers to the bottom row
+        if num <= 3:
+            bottom.append(" ")
+        else:
+            bottom.append(" " + alph[num - 4] + " ")
+    # Finally we combine the two strings and print
+    console.print(final_s + ''.join(bottom))
     return None
     
 
-def play_checkers(board: BoardType, players: Dict[PieceColor, TUIPlayer]) -> None:
+def play_checkers(board: Board, players: Dict[PieceColor, TUIPlayer]) -> None:
     """ Plays a game of Connect Four on the terminal
     Args:
         board: The board to play on
