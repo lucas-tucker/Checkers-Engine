4b17270f1e39f1a3b594911744e86373135c3148
lucast2021 <lucast2021@headroyce.org>
2023-03-03 19:51:00 -0600

    Created mini-max bot but deep copy is not working. Also fixed bugs in bot.py

--- src/bot.py
+++ src/bot.py
@@ -16,7 +16,6 @@ from checkers import Checkers, Board, Piece, Moves, Square, PieceColor
 # - running into error in line 274 of checkers.py code where
 # moving_piece is apparently a NoneType object
 
-
 class Bot:
     """
     Bot that chooses the move with the most points:
@@ -106,15 +105,21 @@ class Bot:
 
         # Add to score to incorporate all information
         score = 0
+        row = mv.location.row
         can_jump = bot._checkers.jump_moves(color)[1]
         if can_jump:
             score += 1
-        if mv in kinging_mvs: #and ind in kinging_mvs[mv]:
+            if mv in long_jump_mvs and ind in long_jump_mvs[mv]:
+                print(f"found long jump move {mv}")
+                score += 3
+        if mv in kinging_mvs and ind in kinging_mvs[mv]:
+            print(f"found king move {mv}")
             score += 5
-        if mv in long_jump_mvs: #and ind in long_jump_mvs[mv]:
-            score += 3
-        if mv in center_mvs: #and ind in center_mvs[mv]:
+        if mv in center_mvs and ind in center_mvs[mv]:
+            print(f"found center move {mv}")
             score += 1
+        if (row == 0 or row == self._checkers._board_dim - 1):
+            score -= 4
         return score
     
     def opposite_color(self, color):
@@ -136,11 +141,11 @@ class Bot:
         first_bot = Bot(sub_game, self._color)
         score = self.get_score(self._color, mv, ind, first_bot, possible_mvs)
 
-        #print("SUBGAME 1")
-        #print(self._checkers)
+        print("SUBGAME 1")
+        print(self._checkers)
         sub_game.execute_single_move(mv, ind)
-        #print("SUBGAME 2")
-        #print(self._checkers)
+        print("SUBGAME 2")
+        print(self._checkers)
 
         opp = self.opposite_color(self._color)
         second_bot = Bot(sub_game, opp)
@@ -202,7 +207,8 @@ class Bot:
 
         Returns: Dict(Move: [int])
         """
-        curr_max = 0 
+        # set max to 1 so we only get multi-jumps
+        curr_max = 1
         jump_dict = {}
         for mv in possible_mvs:
             # Get subtree height and corresponding indices for each Moves obj.
@@ -304,16 +310,14 @@ class Bot:
             ind : int
             center_dict : dict{Moves : list[int]}
         """
-        col = mv.location.col
         # Avoid moving pieces on the ends of the board
-        if (col > 0 and col < self._checkers._board_dim - 1):
-            curr_x = mv.location.col
-            potential_x = mv.children[child_ind].location.col
-            if self.is_toward_center(curr_x, potential_x):
-                if mv in center_dict:
-                    center_dict[mv].append(child_ind)
-                else:
-                    center_dict[mv] = [child_ind]
+        curr_x = mv.location.col
+        potential_x = mv.children[child_ind].location.col
+        if self.is_toward_center(curr_x, potential_x):
+            if mv in center_dict:
+                center_dict[mv].append(child_ind)
+            else:
+                center_dict[mv] = [child_ind]
 
     def kinging_moves(self, move_lst) -> dict:
         """ 
@@ -376,20 +380,30 @@ class Bot:
                 return True
         return False
     
-
 # Testing Code Below
 bot_wins = 0
 rand_wins = 0
 
-for i in range(30):
-    game = Checkers(8)
+for i in range(1):
+    game = Checkers(3)
     black = PieceColor.BLACK
     red = PieceColor.RED
     comp1 = Bot(game, red)
     prev = black
-    while (not game.is_done(red)) and (not game.is_done(black)):
+    j = 2
+    while (not game.is_done(red)) and (not game.is_done(black)) and j>0:
+        print(game)
+        j -= 1
+        print("THE COPY BEFORE EXECUTION: ")
+        cpy = copy.deepcopy(game)
+        print(cpy)
+        move, index, scr = comp1.basic_suggest()
+        cpy.execute_single_move(move, index)
+        print("THE COPY AFTER EXECUTION: ")
+        print(cpy)
+        break
         if prev == black:
-            move, index = comp1.subgame_suggest(depth=3)
+            move, index, scr = comp1.basic_suggest()
             game.execute_single_move(move, index)
             prev = red
         else:
---
+++ src/bot_minimax.py
@@ -0,0 +1,208 @@
+"""
+Bots for Connect-M
+
+(and command for running simulations with bots)
+"""
+import random
+import copy
+import math
+from typing import Union
+
+from checkers import Checkers, Board, Piece, Moves, Square, PieceColor
+
+#
+# Notes:
+# - exit() method in make_random_move class not defined
+# - running into error in line 274 of checkers.py code where
+# moving_piece is apparently a NoneType object
+
+class Move_Tree:
+    """
+    Trees for individual move-index pairs that represent all potential game
+    continuations.
+    """
+    def __init__(self, mv, ind, opp_trees, board_state):
+        self.move = mv
+        self.index = ind
+        self.opp_trees = opp_trees
+        self.board_state = board_state
+
+class Bot:
+    """
+    Minimax bot
+    """
+
+    _checkers: Checkers
+    _color: PieceColor
+
+    def __init__(self, checkers: Checkers, color: PieceColor):
+        """ Constructor
+
+        Args:
+            checkers: Checkers object the bot will use as guide
+            color: Bot's color
+        """
+        self._checkers = checkers
+        self._color = color
+
+    def mini_max(self, depth):
+        """
+        Given a depth level, this method assesses all possible moves and returns
+        the move-index pair which minimizes the worst-case outcome. 
+        """
+        # Get possible moves for this color
+        possible_mvs = self.non_empties(self._checkers.valid_moves(self._color))
+        # Use get_trees to get list of trees corresponding to possible moves
+        tree_list = self.get_trees(possible_mvs, self._color, depth, self._checkers)
+        worst = -math.inf
+        best_mv = None
+        for tree in tree_list:
+            worst_outcome = self.get_worst(tree, self._color)
+            # Find tree with least worst possible outcome
+            if worst_outcome >= worst:
+                best_mv = [tree.move, tree.index]
+        return best_mv
+   
+    def get_worst(self, tree, color):
+        """
+        Given a Move_Tree object and a color, this method determines the worst
+        possible outcome (given by an integer score) from this tree
+        """
+        worst = math.inf
+        # Base case is that tree has no children, so we assess state here
+        if not(tree.opp_trees):
+            return self.assess_state(tree.board_state, color)
+        for subtree in tree.opp_trees:
+            # Recursive call to get closer to the base case of tree leaves 
+            cur_worst = self.get_worst(subtree, color)
+            if cur_worst < worst:
+                worst = cur_worst
+        return worst
+
+    def assess_state(self, board, color):
+        """
+        Given a board and a color, this method returns an assessment (int) of 
+        the board from the standpoint of king and piece counts. 
+        """
+        opp_color = self.opposite_color(color)
+        # Note that valid_moves returns Moves objects for each piece
+        opp_mvs = board.valid_moves(opp_color)
+        mvs = board.valid_moves(color)
+        king_dif = 2 * (self.king_tally(mvs) - self.king_tally(opp_mvs))
+        pieces_dif = len(mvs) - len(opp_mvs)
+        return king_dif + pieces_dif
+
+    def king_tally(self, mvs):
+        """
+        Given a list of move objects, this method returns the total number which
+        correspond to king pieces.
+        """
+        tally = 0
+        for mv in mvs:
+            if mv.location.piece.is_king:
+                tally += 1
+        return tally
+
+    def get_trees(self, mvs, color, depth, board):
+        """
+        Given a list of possible moves, a color, depth, and board, this method
+        returns a tree list (one tree per move-index pair of the mvs list)
+        corresponding to potential game continuations.
+        """
+        opp_color = self.opposite_color(color)
+        tree_list = []
+        for mv in mvs:
+            for ind, child in enumerate(mv.children):
+                new_state = self.simulate_move(board, mv, ind)
+                if depth == 0:
+                    tree_list.append(Move_Tree(mv, ind, [], new_state))
+                else:
+                    # Get all opponent's potential moves in this new state
+                    opp_mvs = self.non_empties(new_state.valid_moves(color))
+                    # Recursive call to make tree list for this move/ind's tree
+                    opp_trees = self.get_trees(opp_mvs, opp_color, depth - 1, new_state)
+                    tree_list.append(Move_Tree(mv, ind, opp_trees, new_state))
+        return tree_list
+    
+    def simulate_move(self, board, mv, ind):
+        """
+        Given a board, move and index, this method returns a new board state
+        (a Checkers object) corresponding to if that move-index were to be 
+        played.
+        """
+        new_state = copy.deepcopy(board)
+        new_state.execute_single_move(mv, ind)
+        return new_state
+    
+    def opposite_color(self, color):
+        if color == PieceColor.RED:
+            return PieceColor.BLACK
+        return PieceColor.RED
+
+    def non_empties(self, mvs):
+        """
+        Given a list of Moves objects, each corresponding to a piece of the
+        bot's color, this method returns a new list of those Moves objects with
+        nonempty children (all valid moves). 
+
+        Returns: list[Moves]
+        """
+        non_empty_lst = []
+        for mv in mvs:
+            if mv.children:
+                non_empty_lst.append(mv)
+        return non_empty_lst
+
+    def to_dict(self, mvs_lst):
+        """
+        Given a list of Moves objects, this method returns a dictionary which 
+        maps each Moves object to its children. 
+
+        Returns: dict{Moves : list[int]}
+        """
+        mv_dict = {}
+        for mv in mvs_lst:
+            mv_dict[mv] = list(range(len(mv.children)))
+        return mv_dict
+
+    def choose_rand(self, move_dict):
+        """ 
+        Given a dictionary which maps Moves to lists of child indices, this
+        method returns a random Move-index tuple corresponding to one move on
+        the board. 
+
+        Args:
+            move_dict: dict{Moves: list[int]}
+        
+        Returns:
+            (Moves, int)
+        """
+        rand_mv = random.choice(list(move_dict.items()))[0]
+        rand_child = random.choice(move_dict[rand_mv])
+        return [rand_mv, rand_child]
+    
+# Testing Code Below
+bot_wins = 0
+rand_wins = 0
+
+for i in range(10):
+    game = Checkers(3)
+    black = PieceColor.BLACK
+    red = PieceColor.RED
+    comp1 = Bot(game, red)
+    prev = black
+    while (not game.is_done(red)) and (not game.is_done(black)):
+        if prev == black:
+            move, index = comp1.mini_max(depth=3)
+            game.execute_single_move(move, index)
+            prev = red
+        else:
+            possible_mvs = comp1.non_empties(comp1._checkers.valid_moves(black))
+            move, index = comp1.choose_rand(comp1.to_dict(possible_mvs))
+            game.execute_single_move(move, index)
+            prev = black
+    if game.is_done(red):
+        rand_wins += 1
+    else:
+        bot_wins += 1
+    print(f"bot won {bot_wins} games, random won {rand_wins} games")
